27a50e635af73425315983dd57c6b918dd69a2f8
Abc <osmnndm@protonmail.com>
2023-04-14 18:59:55 +0200

    m

--- src/test/java/stepdefinitions/CommonStepDefinitions.java
+++ src/test/java/stepdefinitions/CommonStepDefinitions.java
@@ -1,13 +1,40 @@
 package stepdefinitions;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class CommonStepDefinitions {
 
     @Given("kullanici {string} adresine gider")
     public void kullanici_adresine_gider(String string) {
         open(string);
+        // Bir sayfaya gitmek için open() metdodu kullanılıyor. driver.get yerine
+    }
+
+    @And("{int} saniye bekler")
+    public void saniyeBekler(int arg0) {
+        sleep(arg0*1000);
+    }
+
+    @Then("onceki sayfaya gider")
+    public void oncekiSayfayaGider() {
+        back();
+    }
+
+    @Then("sonraki sayfaya gider")
+    public void sonrakiSayfayaGider() {
+        forward();
+    }
+
+    @Then("sayfayi yeniler")
+    public void sayfayiYeniler() {
+        refresh();
+    }
+
+    @Then("sayfayi acik tutar")
+    public void sayfayiAcikTutar() {
     }
 }
--- src/test/resources/features/01_navigations.feature
+++ src/test/resources/features/01_navigations.feature
@@ -2,3 +2,9 @@
 Feature: navigasyonlar
   Scenario: TC 01 navigasyon testi
     Given kullanici "https://www.google.com/" adresine gider
+    Then kullanici "https://www.amazon.com/" adresine gider
+    And 5 saniye bekler
+    Then onceki sayfaya gider
+    Then sonraki sayfaya gider
+    Then sayfayi yeniler
+    Then sayfayi acik tutar
